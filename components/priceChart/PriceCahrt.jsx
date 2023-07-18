55b8d0d8f0f35024cdaf4eb6ae85ac98f965bb5e
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-18 15:44:16 +0330

    .gitignore is now working

--- components/priceChart/PriceCahrt.jsx
+++ components/priceChart/PriceCahrt.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,7 +12,30 @@ import {
   Brush,
   AreaChart,
   Area,
+  ReferenceDot,
+  ComposedChart,
+  Scatter,
+  ResponsiveContainer,
 } from "recharts";
+
+import styles from "./PriceChart.module.css";
+import { useEffect, useState } from "react";
+
+const CustomReferenceDot = (props) => {
+  return (
+    <circle cx={props.cx} r="2" cy={props.cy} fill="#ff8f17">
+      <animate
+        attributeName="r"
+        from="2"
+        to="5"
+        dur="1.5s"
+        begin="0s"
+        repeatCount="indefinite"
+      />
+    </circle>
+  );
+};
+
 const data = [
   {
     name: "Page A",
@@ -27,11 +51,14 @@ const data = [
   },
 ];
 
-import styles from "./PriceChart.module.css";
-
 function PriceChart({ title, current, lower, higher }) {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
-    <div className="container">
+    <div className={`container ${styles.chartContainer}`}>
       <div className={styles.priceContainer}>
         <div>
           <FontAwesomeIcon icon={faCircle} className={styles.lower} />
@@ -43,13 +70,48 @@ function PriceChart({ title, current, lower, higher }) {
         </div>
       </div>
 
-      <AreaChart width={300} height={150} data={data} syncId="anyId">
-        <CartesianGrid strokeDasharray="3 3" />
+      {show && (
+        <div className={styles.chart}>
+          <ResponsiveContainer
+            width="100%"
+            height={150}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 0,
+              left: 0,
+            }}
+          >
+            <AreaChart data={data} syncId="anyId">
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#ff8f17" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#ff8f17" stopOpacity={0} />
+                </linearGradient>
+              </defs>
 
-        <Tooltip />
-        <Line type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-        <Area type="monotone" dataKey="pv" stroke="#f5b22b" fill="#fbae15" />
-      </AreaChart>
+              <Tooltip />
+              <XAxis dataKey="name" />
+              <Area
+                type="monotone"
+                dataKey="pv"
+                stroke="#ff8f17"
+                fillOpacity={1}
+                fill="url(#colorUv)"
+              />
+              <ReferenceDot
+                x={data[data.length - 1].name}
+                y={data[data.length - 1].pv}
+                shape={CustomReferenceDot}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+          <div className={styles.livePriceContainer}>
+            <FontAwesomeIcon icon={faCircle} className={styles.liveDot} />1{" "}
+            {title} = ${current}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
